c39375812d418e31c34cda74dbc9a0ba77302dd3
devinkobe40 <devinkobe40@gmail.com>
2020-11-18 18:39:08 +0800

    final commit of module2

---
+++ module2_solution/assignment2-starter-code/app.js
@@ -0,0 +1,86 @@
+( function (){
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOff', ShoppingListService);
+
+
+  ToBuyController.inject = ['ShoppingListCheckOff'];
+  function ToBuyController(ShoppingListCheckOff){
+    var toBuy = this;
+
+    toBuy.list = ShoppingListCheckOff.getStock();
+    console.log("initial stock: ",toBuy.list.length);
+      // console.log(itemIndex);
+    // toBuy.notifMsg = e.message;
+
+    toBuy.buyItem = function (itemIndex) {
+        ShoppingListCheckOff.addItem(itemIndex);
+    };
+
+
+  }
+
+  AlreadyBoughtController.inject = ['ShoppingListCheckOff'];
+  function AlreadyBoughtController(ShoppingListCheckOff){
+    var alreadyBought = this;
+
+    alreadyBought.items = ShoppingListCheckOff.getAlreadyBought();
+    console.log("Items bought: ", alreadyBought.items.length);
+
+  }
+
+  function ShoppingListService(){
+    var service = this;
+
+    var toBuy = [
+      {
+        name: "Cookies",
+        quantity: "4"
+      },
+      {
+        name: "Tomato",
+        quantity: "10"
+      },
+      {
+        name: "Viagra",
+        quantity: "2"
+      },
+      {
+        name: "Coconut Oil",
+        quantity: "4 "
+      },
+      {
+        name: "Pizza",
+        quantity: "99"
+      },
+
+    ];
+    var alreadyBought = [];
+
+    service.addItem = function (itemIndex) {
+
+          alreadyBought.push(toBuy[itemIndex]);
+          // console.log(itemIndex);
+          toBuy.splice(itemIndex, 1);
+          // console.log("updated stock: ", toBuy.length);
+
+          };
+
+    service.getStock = function () {
+      return toBuy;
+    };
+
+    service.getAlreadyBought = function () {
+      return alreadyBought;
+    };
+
+
+  }
+
+
+}
+
+)();
